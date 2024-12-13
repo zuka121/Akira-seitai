910f24071b29a891ecb0f13246043ba6899886aa
iizuka <iizukahayato1217@gmail.com>
2024-12-13 17:34:57 +0900

    minor

--- mycalendar/settings.py
+++ mycalendar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wt==85a0egr8z=t%ro^p06$yo=g7nm#0s93%srog!z=phm%e-_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
